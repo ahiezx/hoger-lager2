b5a4ce8a767e0f0c13d18241e2aa5b3330e8fbcc
Ahmad Bakdalieh <302461472@student.rocmondriaan.nl>
2022-10-17 12:53:03 +0200

    Finish everything

--- js/main.js
+++ js/main.js
@@ -1,15 +1,23 @@
 console.log("main.js loaded successfully");
-// Higher or lower?
 
 const computerNumber = document.querySelector("#computerNumber");
 const playerNumber = document.querySelector("#playerNumber");
+
+const winDisplay = document.querySelector("#wins");
+const lossDisplay = document.querySelector("#losses");
+const scoreDisplay = document.querySelector("#score");
+
 const message = document.querySelector("#message");
 
-let randomNumber = Math.floor(Math.random() * 100) + 1;
+let wins = 0;
+let losses = 0;
+let score = 10;
+let difficulty = 1;
 
+let playerGuess, computerGuess;
 
-function setComputerNumber() {
-    computerNumber.textContent = randomNumber;
+function setComputerNumber(val) {
+  computerNumber.textContent = val;
 }
 function setPlayerNumber(val) {
   playerNumber.textContent = val;
@@ -19,13 +27,86 @@ function setMessage(msg) {
   message.textContent = msg;
 }
 
-const higher = document.querySelector("#higher").addEventListener("click", function() {
-    let newNumber = Math.floor(Math.random() * 100) + 1;
-    setPlayerNumber(newNumber);
-    if (newNumber > randomNumber) {
-        setMessage("You guessed correctly!");
-        }
-    else {
-        setMessage("You guessed incorrectly!");
-        }
-});
+function addWin() {
+  wins++;
+  winDisplay.textContent = wins;
+  document.querySelector(".container").style.backgroundColor = "green";
+  setScore(score + 5);
+}
+
+function addLoss() {
+  losses++;
+  lossDisplay.textContent = losses;
+  document.querySelector(".container").style.backgroundColor = "red";
+  setScore(score - 3);
+}
+
+function setScore(val) {
+  score = val;
+  scoreDisplay.textContent = score;
+}
+
+
+function getRandomNumber() {
+  return Math.floor(Math.random() * 100) + 1;
+}
+
+function guessHigher() {
+
+  if(score < 1) {
+    setMessage("You don't have enough points to guess higher");
+    return;
+  }
+
+  computerGuess = getRandomNumber();
+  playerGuess = getRandomNumber();
+  setComputerNumber(computerGuess)
+  setPlayerNumber(playerGuess)
+
+  if (playerGuess > computerGuess) {
+    setMessage("You win! Your score has increased 5+ points");
+    addWin();
+  }
+  else {
+    setMessage("You lose! Your score has decreased -3 points");
+    addLoss();
+  }
+
+  
+}
+
+function guessLower () {
+
+  if(score < 1) {
+    setMessage("You don't have enough points to guess lower");
+    return;
+  }
+
+  computerGuess = getRandomNumber();
+  playerGuess = getRandomNumber();
+  setComputerNumber(computerGuess)
+  setPlayerNumber(playerGuess)
+
+  if (playerGuess < computerGuess) {
+    setMessage("You win! Your score has increased 5+ points");
+    addWin();
+  }
+  else {
+    setMessage("You lose! Your score has decreased -3 points");
+    addLoss();
+  }
+  
+
+}
+
+function startGame() {
+  computerGuess = getRandomNumber();
+  setComputerNumber(computerGuess);
+  setPlayerNumber("?");
+  setMessage("Guess Higher or Lower");
+}
+
+startGame();
+
+document.querySelector("#higher").addEventListener("click", guessHigher);
+document.querySelector("#lower").addEventListener("click", guessLower);
